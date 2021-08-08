c1d2eb133cfa4132062fb811649e75e6b249773e
hi <ih>
2021-08-08 20:16:09 +0900

    updated errorhandler

--- trace.py
+++ trace.py
@@ -49,7 +49,7 @@ def page_not_found(e):
     return render_template('404.html'), 404
 
 @app.errorhandler(500)
-def page_not_found(e):
+def server_error(e):
     return render_template('500.html'), 500
 
 if __name__ == '__main__':
