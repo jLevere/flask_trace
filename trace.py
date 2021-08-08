7a4cef026251ecff02eaae1fda0d2356ea3f4778
hi <ih>
2021-08-08 20:06:31 +0900

    added bootstrap

--- trace.py
+++ trace.py
@@ -1,7 +1,10 @@
 from flask import Flask, request, make_response, render_template
+from flask_bootstrap import Bootstrap
 
 import json
 app = Flask(__name__)
+
+bootstrap = Bootstrap(app)
 
 @app.route('/headers/json')
 def json_page():
